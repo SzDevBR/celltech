46a7cd6d470fb778bed091346fb04f0901884287
SzDevBR <81070056+SzDevBR@users.noreply.github.com>
2023-08-18 20:46:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const ejs = require('ejs'); // UPD02
 const serviceRoutes = require('./routes/serviceRoutes');
+const session = require('express-session'); // Adicione esta linha
+const passport = require('passport'); // Adicione esta linha
+const User = require('./models/user'); // Substitua pelo seu modelo de usuário
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,9 +20,25 @@ mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
+// Configuração da sessão
+app.use(
+  session({
+    secret: 'seuSegredoAqui',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+
 app.use(bodyParser.json());
 app.set('view engine', 'ejs'); // Defina o mecanismo de visualização para o EJS
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Serialize e deserialize o usuário
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use('/services', serviceRoutes);
 
