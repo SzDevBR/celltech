a8c2bb68724715fd58aca01af9b4f842eaf2e1e2
SzDevBR <81070056+SzDevBR@users.noreply.github.com>
2023-08-18 20:47:34 -0300

    Update serviceRoutes.js

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -1,9 +1,41 @@
 // src/routes/serviceRoutes.js
 
 const express = require('express');
+const passport = require('passport');
 const serviceController = require('../controllers/serviceController');
 
 const router = express.Router();
+
+// Página de login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Autenticação do técnico
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/services/login',
+    failureFlash: true,
+  })
+);
+
+// Página de dashboard após login bem-sucedido
+router.get('/dashboard', (req, res) => {
+  // Verifique se o usuário está autenticado antes de renderizar a página
+  if (req.isAuthenticated()) {
+    res.render('dashboard', { user: req.user });
+  } else {
+    res.redirect('/services/login');
+  }
+});
+
+// Rota de logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/services/login');
+});
 
 router.get('/create', (req, res) => {
   res.render('createService');
