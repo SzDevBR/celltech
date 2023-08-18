e74c85fdeb2816a50e27b5a7a058131755ede4ba
SzDevBR <81070056+SzDevBR@users.noreply.github.com>
2023-08-18 20:48:39 -0300

    Create user.js

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+// src/models/user.js
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+userSchema.plugin(require('passport-local-mongoose'));
+
+module.exports = mongoose.model('User', userSchema);
