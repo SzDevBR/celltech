ac6df27c8d2d5681611716f43900ec1fe9c227be
SzDevBR <81070056+SzDevBR@users.noreply.github.com>
2023-08-18 20:19:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const ejs = require('ejs'); // UPD02
 const serviceRoutes = require('./routes/serviceRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = 'mongodb+srv://inderux:inderux@cluster0.kwiqp.mongodb.net/celltech-servicos?retryWrites=true&w=majority';
-
+const MONGODB_URI = process.env.MONGODB_URL;
 app.use(bodyParser.json());
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+app.use(bodyParser.json());
+app.set('view engine', 'ejs'); // Defina o mecanismo de visualização para o EJS
+
 
 app.use('/services', serviceRoutes);
 
