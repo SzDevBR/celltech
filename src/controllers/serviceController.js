d4764e38a07c132a032942eb342fd0858d017d96
SzDevBR <81070056+SzDevBR@users.noreply.github.com>
2023-08-18 20:07:56 -0300

    Create serviceController.js

---
+++ src/controllers/serviceController.js
@@ -0,0 +1,54 @@
+// src/controllers/serviceController.js
+
+const Service = require('../models/service');
+
+exports.createService = async (req, res) => {
+  try {
+    // Crie um novo serviço com base nos dados do corpo da solicitação
+    const newService = await Service.create(req.body);
+
+    // Gere um código de serviço com 8 dígitos (simulação)
+    const generatedCode = Math.floor(10000000 + Math.random() * 90000000).toString();
+    newService.code = generatedCode;
+
+    await newService.save();
+
+    res.status(201).json(newService);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar serviço.' });
+  }
+};
+
+exports.getServiceStatus = async (req, res) => {
+  try {
+    const service = await Service.findOne({ code: req.params.code });
+
+    if (!service) {
+      return res.status(404).json({ error: 'Serviço não encontrado.' });
+    }
+
+    res.json(service);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar serviço.' });
+  }
+};
+
+exports.updateService = async (req, res) => {
+  try {
+    const service = await Service.findOne({ code: req.params.code });
+
+    if (!service) {
+      return res.status(404).json({ error: 'Serviço não encontrado.' });
+    }
+
+    // Atualize o status e os comentários do técnico com base nos dados do corpo da solicitação
+    service.status = req.body.status;
+    service.technicianComments = req.body.technicianComments;
+
+    await service.save();
+
+    res.json(service);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao atualizar serviço.' });
+  }
+};
